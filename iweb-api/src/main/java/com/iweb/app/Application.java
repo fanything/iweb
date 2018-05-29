81d105fa4d12e08ee955804dca3e4ed85216d3ed
fanmingxin <f2a87589be339702361bdaf6264eac13>
2018-05-29 21:09:45 +0800

    web启动类配置

--- iweb-api/src/main/java/com/iweb/app/Application.java
+++ iweb-api/src/main/java/com/iweb/app/Application.java
@@ -1,9 +1,22 @@
 package com.iweb.app;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author:FanMingxin
  * @Date: 2018-05-29 下午 08:52
  * @Description:
  */
-public class Application {
+@SpringBootApplication
+public class Application implements CommandLineRunner{
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
